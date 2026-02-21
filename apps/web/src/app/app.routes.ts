86928b321bfd4c266cf246f4db7c6c08394483bd
david-iseeuglycode <david@iseeuglycode.org>
2026-02-21 17:50:24 +0100

    base, test, and 404 pages

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
+import { HomePage } from './pages/home/home';
+import { TestPage } from './pages/test/test';
+import { NotFoundPage } from './pages/not-found/not-found';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', component: HomePage },
+  { path: 'test', component: TestPage },
+  { path: '**', component: NotFoundPage },
+];
---
+++ apps/web/src/app/pages/home/home.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pn-home',
+  templateUrl: './home.html',
+})
+export class HomePage {}
